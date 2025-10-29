4c2579b2aa881b38f76b2997637c11463c668138
Lucien Šíma <lucien.sima@aristotelos.com>
2025-10-29 04:47:15 +0100

    last design changes from Kristyna

--- app.py
+++ app.py
@@ -57,6 +57,5 @@ def get_calculation():
         return {'status': 'invalid'}
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
-    #port = int(os.environ.get("PORT", 8080))
-    #serve(app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 8080))
+    serve(app, host="0.0.0.0", port=port)
